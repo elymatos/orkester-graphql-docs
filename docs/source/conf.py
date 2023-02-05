3be45ed14e63622696a620dc26a51fd5d5bc6eac
Ely Edison Matos <ely.matos@gmail.com>
2023-02-04 23:36:06 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GraphQL/Orkester'
+copyright = '2023, fnzr elymatos'
+author = 'fnzr'
 
 release = '0.1'
 version = '0.1.0'
